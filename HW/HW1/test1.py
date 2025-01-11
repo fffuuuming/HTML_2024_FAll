c4a32d27be06d0851c916f0f0644ac6107acd62c
fuming Ye <benson.oor@gmail.com>
2025-01-11 15:24:36 +0800

    tidy up hw1

--- HW/HW1/test1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.sparse import csr_array
-A = csr_array([[1, 2, 0], [0, 0, 3], [4, 0, 5]])
-# v = csr_array([1, 0, -1])
-# print(A.dot(v.T))
-print(A)
-print(A.toarray())
-print(A.toarray().flatten())
